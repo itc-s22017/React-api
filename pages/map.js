358baddaf8165950e93667482896a2d2bdece366
itc-s22017 <s22017@std.it-college.ac.jp>
2023-02-24 20:45:11 +0000

    2nd commit....

--- pages/map.js
+++ pages/map.js
@@ -1,10 +1,32 @@
-import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api'
-import { useState } from 'react'
-import Container from 'components/container'
+import { GoogleMap, LoadScript, MarkerF, InfoWindow } from '@react-google-maps/api'
+import { useState, useRef } from 'react'
 import icon from 'images-local/favicon.ico'
+import axios from 'axios'
+
+const Marker = (props) => {
+  return (
+    <MarkerF
+      position={props.pos}
+      animation={1}
+      title={props.lng}
+    />
+  )
+}
+
+const Info = (props) => {
+  return (
+    <>
+      <InfoWindow position={props.tokyo}>
+        <div>
+          <h2>{props.title}</h2>
+        </div>
+      </InfoWindow>
+    </>
+  )
+}
 
 const Map = (props) => {
-  const containerStyle = {
+  const style = {
     height: '50vh',
     width: '50%'
   }
@@ -20,12 +42,12 @@ const Map = (props) => {
     <>
       <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_MAPS_KEY}>
         <GoogleMap
-          mapContainerStyle={containerStyle} center={props.pos}
+          mapContainerStyle={style} center={props.pos}
           options={options} onClick={e => props.position(e)}
         >
-          <MarkerF
-            position={props.pos} animation={2} title={props.lng}
-            onClick={(e) => props.removeMarker(e)}
+          <Marker
+            pos={props.pos}
+            lng={props.lng}
           />
         </GoogleMap>
       </LoadScript>
@@ -35,28 +57,45 @@ const Map = (props) => {
 
 const Main = () => {
   const tokyo = {
-    lat: 35.69731,
-    lng: 139.7747
+    lat: 35.65856,
+    lng: 139.745461
   }
 
   const [pos, setPos] = useState(tokyo)
+  const ref = useRef()
+  const [flag, setFlag] = useState(false)
 
   const position = e => {
     setPos(e.latLng)
+    console.log(pos)
   }
 
-  const removeMarker = (e) => {
-    console.log(e)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+          address: ref.current.value,
+          key: process.env.NEXT_PUBLIC_MAPS_KEY
+        }
+      })
+      setPos(response.data.results[0].geometry.location)
+    } catch (error) {
+      console.error('Not Found...')
+    }
   }
 
   return (
-    <Container>
+    <>
       <Map
         position={position} pos={pos}
-        lng={String(`${pos.lat}:${pos.lng}`)}
-        removeMarker={removeMarker}
+        lng={String('a')}
+        tokyo={tokyo}
       />
-    </Container>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input ref={ref} type='text' placeholder='例:東京タワー' />
+      </form>
+    </>
   )
 }
 
